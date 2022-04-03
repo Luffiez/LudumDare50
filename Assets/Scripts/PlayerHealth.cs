3e983f468971448321439d03155f9776486710a7
Erik Rodriguez <Luffiez123@gmail.com>
2022-04-03 20:23:12 +0200

    Vignette

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour, IHurt
@@ -6,15 +8,36 @@ public class PlayerHealth : MonoBehaviour, IHurt
     [SerializeField] private int maxHealth;
     private int currentHealth;
 
+    [SerializeField]
+    VolumeProfile profile;
     public HealthChangedEvent OnHealthChanged;
 
     public int MaxHealth { get => maxHealth; }
     public int CurrentHealth { get => currentHealth; }
 
+    Vignette vignette;
+
+    void Awake()
+    {
+        if (OnHealthChanged == null)
+            OnHealthChanged = new HealthChangedEvent();
+        currentHealth = maxHealth;
+
+        if(profile.TryGet<Vignette>(out var v))
+        {
+            vignette = v;
+        }
+
+        vignette.intensity.value = 0;
+    }
+
     public void NormalDamage(int damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - damage, 0, currentHealth);
         OnHealthChanged?.Invoke(currentHealth, maxHealth);
+
+        float heatlhPerc = 1 - (float)currentHealth / maxHealth;
+        vignette.intensity.value = 0.5f * heatlhPerc;
 
         if (currentHealth <= 0)
         {
@@ -31,12 +54,5 @@ public class PlayerHealth : MonoBehaviour, IHurt
     private void Die()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    void Awake()
-    {
-        if (OnHealthChanged == null)
-            OnHealthChanged = new HealthChangedEvent();
-        currentHealth = maxHealth;
     }
 }
